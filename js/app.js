9176d209fa6267434ed94862958a63b57e51296d
aaaasif <alexasif4@gmail.com>
2021-09-04 15:15:44 +0600

    maybe its done

--- js/app.js
+++ js/app.js
@@ -8,14 +8,14 @@ const searchTemperature = () => {
         .then(data => displayTemperature(data));
 }
 
-const setInnerText = (id, text) => {
-    document.getElementById(id).innerText = text;
-}
+
 
 const displayTemperature = temperature => {
     setInnerText('city', temperature.name);
     setInnerText('temperature', temperature.main.temp);
     setInnerText('condition', temperature.weather[0].main);
     // set weather icon
-    
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
 }
